8be5061ce291fc966efe66f9dc7144d5233beda3
Code-Mahi <hirenjchauhan@hotmail.com>
2022-06-24 12:52:18 +0530

    init
    first init

---
+++ include-header.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TPO</title>
+     <!-- CSS add here -->
+     <link rel="stylesheet" href="public/asstes/plugins/bootstrap-5.2.0/css/bootstrap.min.css" > 
+     <link rel="stylesheet" href="public/asstes/CSS/style.css" >
+     <link rel="stylesheet" href="public/asstes/plugins/fontawesome5pro/css/all.min.css" >
+
+
+      <!-- JS Add here -->
+    <script src="public/asstes/JS/jquery_3_6.js"></script>
+    <script src="public/asstes/JS/popper.min.js"></script>
+    
+    <!-- Plugins add here -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script> -->
+    <script src="public/asstes/plugins/bootstrap-5.2.0/js/bootstrap.min.js"></script>
+</head>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!-- 
+Project Name: TOP
+Author:tarif
+Year:2022
+Description: TOP System
+Company:Selft Study Project
+-->
+
+<!-- Header link Set -->
+<?php include 'include-header.php'; ?>
+
+<!-- Navigation bad set -->
+<?php include 'header.php'; ?>
+
+<!-- Main content -->
+<mian>
+<section class="h-100">
+    <form action="" method="post">
+        <div class="row">
+            <div class="col-md-6 offset-md-3 mt-5">
+                <div class="form-group">
+                    <label for="">Name</label>
+                    <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="">
+                </div>
+                <button name="" id="" class="mt-3 btn btn-primary" href="#" role="button">Submit</button>
+            </div>
+            
+        </div>
+    </form>
+</section>
+</main>
+
+<!-- Footer set here -->
+<?php include 'footer.php'; ?>
+<!-- Footer link set -->
+<?php include 'include-footer.php'; ?>
